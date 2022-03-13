b69e62853f6efd936aad5441f21534905c7159d0
Andrea <tuci.registrazioni@gmail.com>
2022-03-13 15:40:09 +0100

    verso heroku

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
 
     dp.add_error_handler(error)
 
-    PORT = int(os.environ.get('PORT', 5000))
+    PORT = int(os.environ.get('PORT', 443))
     TOKEN = os.getenv('BOTAPIKEY')
     updater.start_webhook(listen='0.0.0.0',
                           port=int(PORT),
